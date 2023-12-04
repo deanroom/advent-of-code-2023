4c3ea8d161277b22c4f0c652788962d3a15c3ce3
JerryDing <deanroom@gmail.com>
2023-12-04 23:19:38 +0800

    solve day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,17 +1,23 @@
-use std::fmt::Display;
+use std::{fmt::Display, process::id};
 
 advent_of_code::solution!(2);
 
-#[derive(Debug)]
-struct GameTry {
+#[derive(Debug, Default)]
+struct Game {
+    id: u32,
+    round: Vec<Round>,
+}
+
+#[derive(Debug, Default)]
+struct Round {
     red: u32,
     green: u32,
     blue: u32,
 }
 
-impl GameTry {
-    fn default() -> GameTry {
-        GameTry {
+impl Round {
+    fn default() -> Round {
+        Round {
             red: 0,
             green: 0,
             blue: 0,
@@ -19,38 +25,8 @@ impl GameTry {
     }
 }
 
-#[derive(Debug)]
-struct Game {
-    id: u32,
-    tries: Vec<GameTry>,
-}
-impl Game {
-    fn default() -> Game {
-        Game {
-            id: 0,
-            tries: Vec::<GameTry>::new(),
-        }
-    }
-}
-
-impl Display for GameTry {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            " Red: {},Green: {},Blue: {}",
-            self.red, self.green, self.blue
-        )
-    }
-}
-
-// impl Display for Game {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "game id:{}, tries:{}", self.id, self.tries)
-//     }
-// }
-
 pub fn part_one(input: &str) -> Option<u32> {
-    let beg = GameTry {
+    let beg = Round {
         red: 12,
         green: 13,
         blue: 14,
@@ -59,64 +35,82 @@ pub fn part_one(input: &str) -> Option<u32> {
     let output: u32 = input
         .lines()
         .map(|line| {
-            let mut token: Vec<&str> = line.split(&[':', ';']).collect();
-            let mut it = token.iter_mut();
-            let game_id: u32 = it
-                .next()
-                .expect("There must game segment")
-                .replace("Game", "")
-                .trim()
-                .parse()
-                .expect("It must be game id.");
-            let mut game = Game::default();
-            game.id = game_id;
-
-            let _: Vec<_> = it
-                .map(|strgame| {
-                    let bolls: Vec<&str> = strgame.split(',').collect();
-                    let mut game_try: GameTry = GameTry::default();
-                    let _: Vec<_> = bolls
-                        .iter()
-                        .map(|boll| {
-                            if boll.contains("red") {
-                                game_try.red += boll
-                                    .replace("red", "")
-                                    .trim()
-                                    .parse::<u32>()
-                                    .expect("red boll number.");
-                            } else if boll.contains("blue") {
-                                game_try.blue += boll
-                                    .replace("blue", "")
-                                    .trim()
-                                    .parse::<u32>()
-                                    .expect("blue boll number.");
-                            } else if boll.contains("green") {
-                                game_try.green += boll
-                                    .replace("green", "")
-                                    .trim()
-                                    .parse::<u32>()
-                                    .expect("green boll number.");
-                            }
-                        })
-                        .collect();
-                    game.tries.push(game_try);
-                })
-                .collect();
-
-            if game.tries.iter().any(|game_try| {
+            let game = process_line(line);
+            if game.round.iter().any(|game_try| {
                 game_try.red > beg.red || game_try.green > beg.green || game_try.blue > beg.blue
             }) {
                 0
             } else {
-                game_id
+                game.id
             }
         })
         .sum();
     Some(output)
 }
 
+fn process_line(line: &str) -> Game {
+    let mut token: Vec<&str> = line.split(&[':', ';']).collect();
+    let mut it = token.iter_mut();
+    let mut game = Game::default();
+    game.id = it
+        .next()
+        .expect("There must game segment")
+        .replace("Game", "")
+        .trim()
+        .parse()
+        .expect("It must be game id.");
+
+    game.round = it
+        .map(|strgame| {
+            let bolls: Vec<&str> = strgame.split(',').collect();
+            let mut game_try: Round = Round::default();
+            let _: Vec<_> = bolls
+                .iter()
+                .map(|boll| {
+                    if boll.contains("red") {
+                        game_try.red += boll
+                            .replace("red", "")
+                            .trim()
+                            .parse::<u32>()
+                            .expect("red boll number.");
+                    } else if boll.contains("blue") {
+                        game_try.blue += boll
+                            .replace("blue", "")
+                            .trim()
+                            .parse::<u32>()
+                            .expect("blue boll number.");
+                    } else if boll.contains("green") {
+                        game_try.green += boll
+                            .replace("green", "")
+                            .trim()
+                            .parse::<u32>()
+                            .expect("green boll number.");
+                    }
+                })
+                .collect();
+            game_try
+        })
+        .collect();
+    game
+}
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let beg = Round {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+
+    let output: u32 = input
+        .lines()
+        .map(|line| {
+            let game = process_line(line);
+            let red = game.round.iter().max_by_key(|x| x.red).unwrap().red;
+            let blue = game.round.iter().max_by_key(|x| x.blue).unwrap().blue;
+            let green = game.round.iter().max_by_key(|x| x.green).unwrap().green;
+            red * blue * green
+        })
+        .sum();
+    Some(output)
 }
 
 #[cfg(test)]
@@ -126,12 +120,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result.unwrap(), 8);
+        assert_eq!(result, Some(8));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2286));
     }
 }
