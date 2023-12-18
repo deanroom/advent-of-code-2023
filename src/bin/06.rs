b40fd1575538db1ef55d9ec44de0cfcc082aa3b8
JerryDing <deanroom@gmail.com>
2023-12-18 20:58:03 +0800

    feat: add day 6 part 1

---
+++ src/bin/06.rs
@@ -0,0 +1,97 @@
+advent_of_code::solution!(6);
+
+use std::slice::Windows;
+
+use nom::{
+    bytes::complete::{take_till, take_until},
+    character::{
+        complete::{self, multispace1, newline, space1},
+        is_digit,
+    },
+    multi::{many1, separated_list1},
+    sequence::{preceded, separated_pair, tuple},
+    IResult, Parser,
+};
+use nom_supreme::{tag::complete::tag, ParserExt};
+
+#[derive(Debug)]
+struct Race {
+    time: u32,
+    distance: u32,
+}
+
+impl Race {
+    fn get_wins(&self) -> u32 {
+        let mut wins: Vec<u32> = vec![];
+        for hold in 1..self.time {
+            if (self.time - hold) * hold > self.distance {
+                wins.push(hold);
+            }
+        }
+        dbg!(wins.len() as u32)
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (_, races) = parse(input).expect("A valid parse");
+    let races = gen_races(races);
+    let sum = races.iter().fold(1, |acc, x| acc * x.get_wins());
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+fn parse<'a>(input: &'a str) -> IResult<&'a str, (Vec<u32>, Vec<u32>)> {
+    let (input, time) = tag("Time:")
+        .precedes(multispace1)
+        .precedes(separated_list1(space1, complete::u32))
+        .parse(input)?;
+
+    let (input, distance) = newline
+        .precedes(tag("Distance:"))
+        .precedes(multispace1)
+        .precedes(separated_list1(space1, complete::u32))
+        .parse(input)?;
+    Ok((input, (time, distance)))
+}
+fn gen_races(input: (Vec<u32>, Vec<u32>)) -> Vec<Race> {
+    let races: Vec<Race> = input
+        .0
+        .iter()
+        .zip(input.1.iter())
+        .map(|item| Race {
+            time: *item.0,
+            distance: *item.1,
+        })
+        .collect();
+    races
+}
+
+fn parser(input: &str) -> IResult<&str, &str> {
+    space1(input)
+}
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        let (input, (time, distance)) = parse(input).expect("A valid parse");
+        assert_eq!(time, vec![7, 15, 30]);
+        assert_eq!(distance, vec![9, 40, 200]);
+    }
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(288));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
