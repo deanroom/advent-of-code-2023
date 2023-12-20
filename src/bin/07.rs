81a239a54ff03a4ef693c23372400bbc5ddb1898
JerryDing <deanroom@gmail.com>
2023-12-20 17:09:59 +0800

    add day 7

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,44 +1,56 @@
 advent_of_code::solution!(7);
 
-use std::cmp::Ordering;
-
 use nom::character::complete::{self, alphanumeric1, space1};
-
 use nom::sequence::tuple;
 use nom::IResult;
 
-#[derive(Debug)]
-// enum Rank {
-//     FiveOfAKind,
-//     FourOfAKing,
-//     FullHouse,
-//     ThreeOfAKind,
-//     TwoPair,
-//     OnePair,
-//     HighCard,
-//     None,
-// }
+#[derive(Debug, Clone, Copy)]
+enum Rank {
+    FiveOfAKind = 7,
+    FourOfAKind = 6,
+    FullHouse = 5,
+    ThreeOfAKind = 4,
+    TwoPair = 3,
+    OnePair = 2,
+    HighCard = 1,
+    None = 0,
+}
 struct CamelCard {
     //TODO string or str
     hand: String,
+    origin_hand: String,
     bid: u32,
     groups: Vec<String>,
-    power: u8,
+    power: Rank,
+    power2: Rank,
+    score_hand: (u8, u8, u8, u8, u8),
     rank: u32,
 }
 
 impl CamelCard {
     fn new(hand: String, bid: u32) -> CamelCard {
         let mut card = CamelCard {
-            hand,
+            hand: String::from(&hand),
             bid,
             groups: vec![],
-            power: 0,
+            power: Rank::None,
+            power2: Rank::None,
             rank: 0,
+            origin_hand: hand,
+            score_hand: (0, 0, 0, 0, 0),
         };
         card.order_cards();
+        let mut score = card.origin_hand.chars().map(|c| card.get_order(&c));
+        card.score_hand = (
+            score.next().unwrap(),
+            score.next().unwrap(),
+            score.next().unwrap(),
+            score.next().unwrap(),
+            score.next().unwrap(),
+        );
         card.group();
         card.power();
+        card.power2();
         card
     }
 
@@ -51,9 +63,9 @@ impl CamelCard {
     fn get_order(&self, c: &char) -> u8 {
         match c {
             'A' => 14,
-            'Q' => 13,
-            'K' => 12,
-            'J' => 11,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 1,
             'T' => 10,
             _ => c.to_digit(10).expect("should be number") as u8,
         }
@@ -88,25 +100,83 @@ impl CamelCard {
             .max_by_key(|x| x.len())
             .expect("group")
             .len();
-
         self.power = match max_len {
-            5 => 7,
-            4 => 6,
+            5 => Rank::FiveOfAKind,
+            4 => Rank::FourOfAKind,
             3 => {
                 if self.groups.len() == 2 {
-                    5
+                    Rank::FullHouse
                 } else {
-                    4
+                    Rank::ThreeOfAKind
                 }
             }
             2 => {
                 if self.groups.len() == 3 {
-                    3
+                    Rank::TwoPair
                 } else {
-                    2
+                    Rank::OnePair
                 }
             }
-            1 => 1,
+            1 => Rank::HighCard,
+            _ => panic!("WRONG."),
+        };
+    }
+    fn power2(&mut self) {
+        let max_len = self
+            .groups
+            .iter()
+            .max_by_key(|x| x.len())
+            .expect("group")
+            .len();
+        let joker = "J";
+        self.power2 = match max_len {
+            5 => Rank::FiveOfAKind,
+            4 => {
+                if self.hand.contains(joker) {
+                    Rank::FiveOfAKind
+                } else {
+                    Rank::FourOfAKind
+                }
+            }
+            3 => {
+                if self.groups.len() == 2 {
+                    if self.hand.contains(joker) {
+                        Rank::FiveOfAKind
+                    } else {
+                        Rank::FullHouse
+                    }
+                } else {
+                    if self.hand.contains(joker) {
+                        Rank::FourOfAKind
+                    } else {
+                        Rank::ThreeOfAKind
+                    }
+                }
+            }
+            2 => {
+                if self.groups.len() == 3 {
+                    if self.groups[0].contains(joker) || self.groups[1].contains(joker) {
+                        Rank::FourOfAKind
+                    } else if self.groups[2].contains(joker) {
+                        Rank::FullHouse
+                    } else {
+                        Rank::TwoPair
+                    }
+                } else {
+                    if self.hand.contains(joker) {
+                        Rank::ThreeOfAKind
+                    } else {
+                        Rank::OnePair
+                    }
+                }
+            }
+            1 => {
+                if self.hand.contains(joker) {
+                    Rank::OnePair
+                } else {
+                    Rank::HighCard
+                }
+            }
             _ => panic!("WRONG."),
         };
     }
@@ -114,7 +184,6 @@ impl CamelCard {
 
 fn parse_line(input: &str) -> IResult<&str, CamelCard> {
     let (input, (hand, _, bid)) = tuple((alphanumeric1, space1, complete::u32))(input)?;
-
     Ok((input, CamelCard::new(hand.to_string(), bid)))
 }
 fn parse(input: &str) -> Vec<CamelCard> {
@@ -129,38 +198,40 @@ fn parse(input: &str) -> Vec<CamelCard> {
 }
 pub fn part_one(input: &str) -> Option<u32> {
     let mut cards = parse(input);
-    cards.sort_by(|a, b| {
-        let zip: Vec<(char, char)> = a.hand.chars().zip(b.hand.chars()).collect();
-        let order = match a.power.cmp(&b.power) {
-            Ordering::Equal => {
-                let mut order = Ordering::Equal;
-                for (x, y) in zip.iter() {
-                    match a.get_order(x).cmp(&b.get_order(y)) {
-                        Ordering::Equal => continue,
-                        some_order => {
-                            order = some_order;
-                            break;
-                        }
-                    }
-                }
-                order
-            }
-            power_order => power_order,
-        };
-        order
-    });
+    cards.sort_unstable_by_key(|x| (x.power as u8, x.score_hand));
 
     for (index, ele) in cards.iter_mut().enumerate() {
         ele.rank = index as u32 + 1;
     }
+    // cards.iter().for_each(|card| {
+    //     println!(
+    //         "{},{:?},{},{}",
+    //         card.origin_hand, card.power, card.bid, card.rank
+    //     )
+    // });
 
     let result = cards.iter().map(|x| x.bid * x.rank).sum::<u32>();
 
     Some(result)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut cards = parse(input);
+    cards.sort_unstable_by_key(|x| (x.power2 as u8, x.score_hand));
+
+    for (index, ele) in cards.iter_mut().enumerate() {
+        ele.rank = index as u32 + 1;
+    }
+    // cards.iter().for_each(|card| {
+    //     println!(
+    //         "{},{:?},{},{}",
+    //         card.origin_hand, card.power2, card.bid, card.rank
+    //     )
+    // });
+
+    let result = cards.iter().map(|x| x.bid * x.rank).sum::<u32>();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -171,7 +242,7 @@ mod tests {
     fn test_parse() {
         let input = &advent_of_code::template::read_file("examples", DAY);
         let mut cards = parse(input);
-        cards.sort_unstable_by_key(|a| a.power);
+        //cards.sort_unstable_by_key(|a| a.power);
 
         for (index, ele) in cards.iter_mut().enumerate() {
             ele.rank = index as u32 + 1;
@@ -195,6 +266,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5905));
     }
 }
