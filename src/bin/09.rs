fc1952e6ae0126ac2d43e0e8b470f0412886c0d5
JerryDing <deanroom@gmail.com>
2023-12-21 20:19:01 +0800

    add day part 1

---
+++ src/bin/09.rs
@@ -0,0 +1,77 @@
+advent_of_code::solution!(9);
+
+fn parse(input: &str) -> Vec<Vec<i32>> {
+    let output: Vec<Vec<i32>> = input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let line_number: Vec<i32> = line
+                .split(' ')
+                .map(|x| x.parse().expect("must be number."))
+                .collect();
+            line_number
+        })
+        .collect();
+    output
+}
+
+fn fill_number(input: &Vec<i32>) -> i32 {
+    let mut count = *input.last().expect("must be a number.");
+    let output: Vec<i32> = input
+        .iter()
+        .enumerate()
+        .filter(|x| x.0 > 0)
+        .map(|(index, num)| *num - input[index - 1])
+        .collect();
+    if output.iter().any(|x| *x != 0) {
+        count = count + fill_number(&output)
+    }
+    println!("{:?}", output);
+    count
+}
+
+pub fn part_one(input: &str) -> Option<i32> {
+    let input = parse(input);
+    let result = input.iter().fold(0, |acc, x| {
+        let output = fill_number(x);
+        acc + output
+    });
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fill_number() {
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        let input = parse(input);
+        input.iter().for_each(|x| {
+            let output = fill_number(x);
+            println!("{}", output);
+        });
+    }
+
+    #[test]
+    fn test_parse() {
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        let output = parse(input);
+        println!("{:?}", output);
+    }
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(114));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
