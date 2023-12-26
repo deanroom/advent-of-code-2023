906b0a50d39b3e51d8c4163137768a7a128b3ecb
JerryDing <deanroom@gmail.com>
2023-12-26 19:57:39 +0800

    chore add day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,53 +1,32 @@
 advent_of_code::solution!(10);
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 enum Direction {
-    None = 0b0000,
-    Up = 0b0001,
-    Down = 0b0010,
-    Left = 0b0100,
-    Right = 0b1000,
+    North = 0b0001,
+    South = 0b0010,
+    West = 0b0100,
+    East = 0b1000,
 }
 
-impl Clone for Direction {
-    fn clone(&self) -> Self {
-        match self {
-            Self::None => Self::None,
-            Self::Up => Self::Up,
-            Self::Down => Self::Down,
-            Self::Left => Self::Left,
-            Self::Right => Self::Right,
-        }
-    }
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum PipeType {
+    Vertical,
+    Horizontal,
+    NorthEast,
+    NorthWest,
+    SouthWest,
+    SouthEast,
+    StartingPosition,
+    Ground,
 }
 
-impl PartialEq for Direction {
-    fn eq(&self, other: &Self) -> bool {
-        core::mem::discriminant(self) == core::mem::discriminant(other)
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node {
     position: (i32, i32),
-    next_direction: Direction,
-    previous_direction: Direction,
+    pipe_type: PipeType,
     outer_direction: Vec<Direction>,
     inner_direction: Vec<Direction>,
     next: Option<Box<Node>>,
-}
-
-impl Clone for Node {
-    fn clone(&self) -> Self {
-        Self {
-            outer_direction: self.outer_direction.clone(),
-            inner_direction: self.inner_direction.clone(),
-            next: self.next.clone(),
-            next_direction: self.next_direction.clone(),
-            previous_direction: self.previous_direction.clone(),
-            position: self.position,
-        }
-    }
 }
 
 impl Node {
@@ -56,8 +35,9 @@ impl Node {
             outer_direction: vec![],
             inner_direction: vec![],
             next: None,
-            next_direction: Direction::None,
-            previous_direction: Direction::None,
+            pipe_type: PipeType::Ground,
+            // next_direction: Direction::None,
+            // previous_direction: Direction::None,
             position: (0, 0),
         }
     }
@@ -66,13 +46,7 @@ impl Node {
         let output = self
             .get_directions()
             .iter()
-            .filter(move |x: &&Direction| match x {
-                Direction::Up => other.iter().any(|y| y == &Direction::Down),
-                Direction::Down => other.iter().any(|y| y == &Direction::Up),
-                Direction::Left => other.iter().any(|y| y == &Direction::Right),
-                Direction::Right => other.iter().any(|y| y == &Direction::Left),
-                _ => false,
-            })
+            .filter(move |x: &&Direction| other.iter().any(|y| *y == self.reverse_direction(x)))
             .map(|x| (x.clone(), self.reverse_direction(x)))
             .collect();
         output
@@ -82,55 +56,35 @@ impl Node {
         let neighbors: Vec<Node> = nodes
             .iter()
             .filter_map(|x| {
-                let mut node = Node::new();
-                node.position = x.position;
-                node.outer_direction = x.outer_direction.clone();
-                node.inner_direction = x.inner_direction.clone();
-
-                let mut result: Option<Node> = None;
-                let mut direction = Direction::None;
+                let node = x.clone();
                 let position = x.position;
                 let possible_directions = self.try_connect(&node);
 
                 if position == (self.position.0 - 1, self.position.1)
-                    && possible_directions.iter().any(|x| x.0 == Direction::Up)
+                    && possible_directions.iter().any(|x| x.0 == Direction::North)
                 {
-                    direction = Direction::Up;
-                }
-
-                if position == (self.position.0 + 1, self.position.1)
-                    && possible_directions.iter().any(|x| x.0 == Direction::Down)
+                    Some(node)
+                } else if position == (self.position.0 + 1, self.position.1)
+                    && possible_directions.iter().any(|x| x.0 == Direction::South)
                 {
-                    direction = Direction::Down;
-                }
-
-                if position == (self.position.0, self.position.1 + 1)
-                    && possible_directions.iter().any(|x| x.0 == Direction::Right)
+                    Some(node)
+                } else if position == (self.position.0, self.position.1 + 1)
+                    && possible_directions.iter().any(|x| x.0 == Direction::East)
                 {
-                    direction = Direction::Right;
-                }
-
-                if position == (self.position.0, self.position.1 - 1)
-                    && possible_directions.iter().any(|x| x.0 == Direction::Left)
+                    Some(node)
+                } else if position == (self.position.0, self.position.1 - 1)
+                    && possible_directions.iter().any(|x| x.0 == Direction::West)
                 {
-                    direction = Direction::Left;
+                    Some(node)
+                } else {
+                    None
                 }
-                
-                if direction != Direction::None {
-                    self.next_direction = direction.clone();
-                    node.previous_direction = self.reverse_direction(&direction);
-                    result = Some(node);
-                }
-
-                result
             })
             .collect();
 
         if !neighbors.is_empty() {
             let next = neighbors[0].clone();
-            if neighbors.len() == 2 {
-                self.previous_direction = neighbors[1].previous_direction.clone();
-            }
+
             self.next = Some(Box::new(next));
             let nodes: Vec<Node> = nodes
                 .into_iter()
@@ -138,25 +92,14 @@ impl Node {
                 .collect();
 
             self.next.as_mut().expect("").link_nodes(nodes);
-        } else {
-            let directions = self.get_directions();
-            let direction = directions
-                .iter()
-                .find(|x| **x != self.next_direction)
-                .expect("node.");
-            self.next_direction = direction.clone();
         }
         self
     }
+
     fn is_start(&self) -> bool {
-        self.inner_direction
-            == vec![
-                Direction::Up,
-                Direction::Down,
-                Direction::Left,
-                Direction::Right,
-            ]
+        self.pipe_type == PipeType::StartingPosition
     }
+
     fn get_directions(&self) -> Vec<Direction> {
         let matches = self
             .outer_direction
@@ -167,11 +110,10 @@ impl Node {
     }
     fn reverse_direction(&self, direction: &Direction) -> Direction {
         match direction {
-            Direction::Up => Direction::Down,
-            Direction::Down => Direction::Up,
-            Direction::Left => Direction::Right,
-            Direction::Right => Direction::Left,
-            Direction::None => Direction::None,
+            Direction::North => Direction::South,
+            Direction::South => Direction::North,
+            Direction::West => Direction::East,
+            Direction::East => Direction::West,
         }
     }
 }
@@ -184,41 +126,47 @@ fn parse(input: &str) -> Vec<Vec<Node>> {
         let mut nodes_row: Vec<Node> = vec![];
         for (c, col) in row.chars().enumerate() {
             let mut node = Node::new();
-            node.inner_direction = parse_direction(col);
+            let (direction, pip_type) = parse_direction(col);
+            node.inner_direction = direction;
             node.position = (r as i32, c as i32);
             if r > 0 {
-                node.outer_direction.push(Direction::Up);
+                node.outer_direction.push(Direction::North);
             }
             if r < row_len - 1 {
-                node.outer_direction.push(Direction::Down);
+                node.outer_direction.push(Direction::South);
             }
             if c > 0 {
-                node.outer_direction.push(Direction::Left);
+                node.outer_direction.push(Direction::West);
             }
             if c < col_len - 1 {
-                node.outer_direction.push(Direction::Right);
+                node.outer_direction.push(Direction::East);
             }
+            node.pipe_type = pip_type;
             nodes_row.push(node);
         }
         nodes.push(nodes_row);
     }
     nodes
 }
-fn parse_direction(c: char) -> Vec<Direction> {
+
+fn parse_direction(c: char) -> (Vec<Direction>, PipeType) {
     match c {
-        '|' => vec![Direction::Up, Direction::Down],
-        '-' => vec![Direction::Left, Direction::Right],
-        'L' => vec![Direction::Up, Direction::Right],
-        'J' => vec![Direction::Up, Direction::Left],
-        '7' => vec![Direction::Down, Direction::Left],
-        'F' => vec![Direction::Down, Direction::Right],
-        '.' => vec![],
-        'S' => vec![
-            Direction::Up,
-            Direction::Down,
-            Direction::Left,
-            Direction::Right,
-        ],
+        '|' => (vec![Direction::North, Direction::South], PipeType::Vertical),
+        '-' => (vec![Direction::West, Direction::East], PipeType::Horizontal),
+        'L' => (vec![Direction::North, Direction::East], PipeType::NorthEast),
+        'J' => (vec![Direction::North, Direction::West], PipeType::NorthWest),
+        '7' => (vec![Direction::South, Direction::West], PipeType::SouthWest),
+        'F' => (vec![Direction::South, Direction::East], PipeType::SouthEast),
+        '.' => (vec![], PipeType::Ground),
+        'S' => (
+            vec![
+                Direction::North,
+                Direction::South,
+                Direction::West,
+                Direction::East,
+            ],
+            PipeType::StartingPosition,
+        ),
         _ => panic!("wrong char."),
     }
 }
@@ -240,31 +188,59 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some((count + 1) / 2)
 }
 
+fn is_point_in_polygon(p: (i32, i32), polygon: &[(i32, i32)]) -> bool {
+    let n = polygon.len();
+    let mut inside = false;
+
+    for i in 0..n {
+        let j = (i + 1) % n;
+
+        let vi = &polygon[i];
+        let vj = &polygon[j];
+
+        // 检查射线与多边形边的交点
+        if (vi.1 > p.1) != (vj.1 > p.1)
+            && p.0 < ((vj.0 - vi.0) * (p.1 - vi.1) / (vj.1 - vi.1) + vi.0)
+        {
+            inside = !inside;
+        }
+    }
+    inside
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
     let output = parse(input);
-    let nodes: Vec<Node> = output.into_iter().flatten().collect();
-    let mut start_node: Node = nodes
+    let all_nodes: Vec<Node> = output.into_iter().flatten().collect();
+    let mut start_node: Node = all_nodes
         .iter()
         .find(|x| x.is_start())
-        .expect("must ind start node.")
+        .expect("must in start node.")
         .clone();
-    let mut root = start_node.link_nodes(nodes);
-    let mut nodes = vec![root];
+    let mut root = start_node.link_nodes(all_nodes.clone());
+    let mut nodes_in_graph = vec![root];
     while root.next.is_some() {
         root = root.next.as_ref().expect("node");
-        nodes.push(root);
+        nodes_in_graph.push(root);
     }
-    nodes
-        .iter()
-        // .filter(|x| *x.inner_direction == vec![Direction::Down, Direction::Right])
-        .for_each(|x| {
-            println!(
-                "{:?}, {:?}, {:?}",
-                x.position, x.previous_direction, x.next_direction
-            );
-        });
 
-    None
+    let nodes_in_graph: Vec<(i32, i32)> = nodes_in_graph
+        .iter()
+        .map(|p| (p.position.1, p.position.0))
+        .collect();
+
+    let result: Vec<(i32, i32)> = all_nodes
+        .iter()
+        .filter(|x| !nodes_in_graph.contains(&(x.position.1, x.position.0)))
+        .map(|x| (x.position.1, x.position.0))
+        .filter(|x| is_point_in_polygon(*x, &nodes_in_graph[..]))
+        .collect();
+
+    println!("{:?}", result);
+    // nodes.iter().for_each(|x| {
+    //     println!("{:?},{:?}", x.position, x.pipe_type);
+    // });
+
+    Some(result.len() as u32)
 }
 
 #[cfg(test)]
@@ -295,8 +271,24 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_part_two_1() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(4));
+    }
+    #[test]
+    fn test_part_two_2() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
+        assert_eq!(result, Some(8));
+    }
+    #[test]
+    fn test_part_two_3() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 4,
+        ));
+        assert_eq!(result, Some(10));
     }
 }
