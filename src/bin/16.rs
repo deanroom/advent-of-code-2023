10b6eeb64707c09464b5797ae40027326422e8f1
JerryDing <deanroom@gmail.com>
2024-01-29 23:47:56 +0800

    solve day 16

---
+++ src/bin/16.rs
@@ -0,0 +1,217 @@
+use std::collections::{BTreeMap, HashMap, HashSet};
+
+use itertools::{Itertools, Position};
+use linked_hash_map::{Entry, LinkedHashMap};
+use num_complex::Complex;
+advent_of_code::solution!(16);
+
+fn get_next_positions(
+    position: (Complex<i32>, Complex<i32>, char),
+) -> Vec<(Complex<i32>, Complex<i32>)> {
+    let (pos, dir, c) = position;
+    let mut positions = vec![];
+
+    if dir == Complex::new(0, 1) {
+        match c {
+            '|' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            '-' => {
+                let new_dir = Complex::new(-1, 0);
+                positions.push((pos + new_dir, new_dir));
+                let new_dir: Complex<i32> = Complex::new(1, 0);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '/' => {
+                let new_dir: Complex<i32> = Complex::new(1, 0);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '\\' => {
+                let new_dir: Complex<i32> = Complex::new(-1, 0);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '.' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            _ => panic!("Invalid char"),
+        }
+    } else if dir == Complex::new(0, -1) {
+        match c {
+            '|' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            '-' => {
+                let new_dir = Complex::new(-1, 0);
+                positions.push((pos + new_dir, new_dir));
+                let new_dir: Complex<i32> = Complex::new(1, 0);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '/' => {
+                let new_dir: Complex<i32> = Complex::new(-1, 0);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '\\' => {
+                let new_dir: Complex<i32> = Complex::new(1, 0);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '.' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            _ => panic!("Invalid char"),
+        }
+    } else if dir == Complex::new(1, 0) {
+        match c {
+            '|' => {
+                let new_dir = Complex::new(0, 1);
+                positions.push((pos + new_dir, new_dir));
+                let new_dir: Complex<i32> = Complex::new(0, -1);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '-' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            '/' => {
+                let new_dir: Complex<i32> = Complex::new(0, 1);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '\\' => {
+                let new_dir: Complex<i32> = Complex::new(0, -1);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '.' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            _ => panic!("Invalid char"),
+        }
+    } else if dir == Complex::new(-1, 0) {
+        match c {
+            '|' => {
+                let new_dir = Complex::new(0, 1);
+                positions.push((pos + new_dir, new_dir));
+                let new_dir: Complex<i32> = Complex::new(0, -1);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '-' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            '/' => {
+                let new_dir: Complex<i32> = Complex::new(0, -1);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '\\' => {
+                let new_dir: Complex<i32> = Complex::new(0, 1);
+                positions.push((pos + new_dir, new_dir));
+            }
+            '.' => {
+                let new_dir = dir;
+                positions.push((pos + new_dir, new_dir));
+            }
+            _ => panic!("Invalid char"),
+        }
+    }
+    positions
+}
+
+fn get_positions(
+    position: (Complex<i32>, Complex<i32>),
+    map: &LinkedHashMap<Complex<i32>, char>,
+    seen: &mut HashSet<(Complex<i32>, Complex<i32>)>,
+) {
+    if let Some(c) = map.get(&position.0) {
+        if !seen.insert(position) {
+            return;
+        }
+        let positions = get_next_positions((position.0, position.1, *c));
+        if positions.is_empty() {
+            return;
+        }
+        for (pos, dir) in positions {
+            get_positions((pos, dir), map, seen);
+        }
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut map: LinkedHashMap<Complex<i32>, char> = LinkedHashMap::new();
+    let mut seen: HashSet<(Complex<i32>, Complex<i32>)> = HashSet::new();
+    input.lines().enumerate().for_each(|(i, line)| {
+        line.chars().enumerate().for_each(|(j, c)| {
+            map.insert(Complex::new(j as i32, -1 * i as i32), c);
+        })
+    });
+    let start_position = Complex::new(0, 0);
+    let start_dir = Complex::new(1, 0);
+    get_positions((start_position, start_dir), &map, &mut seen);
+    Some(seen.iter().map(|x| x.0).unique().count() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut map: LinkedHashMap<Complex<i32>, char> = LinkedHashMap::new();
+    input.lines().enumerate().for_each(|(i, line)| {
+        line.chars().enumerate().for_each(|(j, c)| {
+            map.insert(Complex::new(j as i32, -1 * i as i32), c);
+        })
+    });
+    let height = input.lines().count() as i32;
+    let width = input.lines().next().unwrap().chars().count() as i32;
+
+    let mut start_positions = HashSet::new();
+
+    map.keys().filter(|x| x.re == 0).for_each(|k| {
+        start_positions.insert((k, Complex::new(1, 0)));
+    });
+
+    map.keys().filter(|x| x.re == 0).for_each(|k| {
+        start_positions.insert((k, Complex::new(1, 0)));
+    });
+
+    map.keys().filter(|x| x.re == width - 1).for_each(|k| {
+        start_positions.insert((k, Complex::new(-1, 0)));
+    });
+
+    map.keys().filter(|x| x.im == 0).for_each(|k| {
+        start_positions.insert((k, Complex::new(0, -1)));
+    });
+
+    map.keys()
+        .filter(|x| x.im == -1 * height + 1)
+        .for_each(|k| {
+            start_positions.insert((k, Complex::new(0, 1)));
+        });
+    let output = start_positions
+        .iter()
+        .map(|(&pos, dir)| {
+            let mut seen: HashSet<(Complex<i32>, Complex<i32>)> = HashSet::new();
+            get_positions((pos, *dir), &map, &mut seen);
+            let output = seen.iter().map(|x| x.0).unique().count() as u32;
+            output
+        })
+        .max()
+        .unwrap();
+
+    Some(output)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(46));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result: Option<u32> = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(51));
+    }
+}
